622d711871ec8a7cd0a441f0390dbf3ae14c9513
Jonatas Afonso <jonatas_afonso@hotmail.com>
2018-04-02 18:41:21 +0100

    Atualização

---
+++ Validadores/Validadores/GlobalLanguageOfBusiness/Ean13.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Validadores.GlobalLanguageOfBusiness
+{
+    public class Ean13 : Validador
+    {
+        public override bool EstaValido(string parametros)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GerarValorParaTestes(bool formatado = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToStringFormatado(string parametros)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Validadores/Validadores/Validador.cs
+++ Validadores/Validadores/Validador.cs
@@ -16,10 +16,10 @@ namespace Validadores
         {
             var elementosDiferentes = parametros.ToCharArray().ToList();
             var resultado = elementosDiferentes.Distinct().Count() > 1;
-            
+
             return resultado;
         }
-        
+
         protected virtual bool VerificarDigitos(string parametros, int posicaoDigito)
         {
             var digitoVerificadorCalculado = CalcularDigitoVerificador(parametros, posicaoDigito);
@@ -35,7 +35,27 @@ namespace Validadores
         }
 
 
-        protected abstract int CalcularDigitoVerificador(string parametros, int posicaoDigito);
+        protected virtual int CalcularDigitoVerificador(string parametros, int posicaoDigito)
+        {
+            var somatorio = 0;
+            var multiplicador = posicaoDigito + 1;
+
+            for (var i = 0; i < posicaoDigito; i++)
+            {
+                var elemento = Convert.ToInt32(parametros[i].ToString());
+                var va = elemento * multiplicador;
+                somatorio += va;
+                multiplicador--;
+            }
+
+            var resto = somatorio % 11;
+            var digitoVerificadorCalculado = 0;
+
+            if (resto >= 2)
+                digitoVerificadorCalculado = 11 - resto;
+            return digitoVerificadorCalculado;
+        }
+
         public abstract bool EstaValido(string parametros);
         public abstract string GerarValorParaTestes(bool formatado = false);
         public abstract string ToStringFormatado(string parametros);
